456f6a1bf8cb445d95d464e353570cc8274c3a01
David Hall <david@davidhalldevelops.com>
2021-09-13 10:26:39 -0400

    created subdirs (includes, img) began index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+        <title>Dauphin Motor Company - Home Page</title>
+        <meta name="author" content="David Hall">
+        <meta name="description" content="A mock-up for a New Motor parts company website - to include product catalogs, description pages, order pages, login system, and more.">
+        <meta property="og:image" content="img/dauphinLogo.jpg">
+        <meta property="og:description" content="Dauphin Motor Company - Local Excellence.">
+        <meta property="og:title" content="Dauphin Motor Company">
+        <link rel="icon" href="img/dauhpinLogoIcon.png" type="image/x-icon">
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">        
+    </head>
+    <body>
+        <?php include 'includes/sections/header.html' ?>
+        <main>
+            <section class="banner-image-wrap">
+                <img src="img/dauphinLogo.jpg" alt="Dauphin Motor Company - Logo">
+                <span class="logo-text raleway" >Motor Company</span>
+            </section>
+            <section class="tagline"></section>
+        </main>
+        <?php include 'includes/sections/footer.html'?>
+    </body>
+</html>
